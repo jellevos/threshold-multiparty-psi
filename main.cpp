e65cacee152691a20844db4ad2a9a1089989131e
Jelle Vos <j.v.jelle@gmail.com>
2020-10-07 17:25:37 +0200

    Clean up

--- main.cpp
+++ main.cpp
@@ -17,25 +17,15 @@ public:
     void insert(unsigned long element) {
         std::cout << "insert" << std::endl;
         for (unsigned long i = 0; i < this->k_hashes; ++i) {
-            unsigned long h = BloomFilter::hash(element, i);
-            //std::cout << "hash(" << element << ", " << i << "): " << h << std::endl;
-            unsigned long index = h % this->m_bits;
-            //std::cout << "insert index: " << index << std::endl;
+            unsigned long index = BloomFilter::hash(element, i) % this->m_bits;
             this->storage.at(index) = true;
         }
     }
 
     /// Checks whether an element appears to have been inserted into the Bloom filter
     bool contains(unsigned long element) {
-        //std::cout << "contains" << std::endl;
         for (unsigned long i = 0; i < this->k_hashes; ++i) {
-//            std::cout << "containing" << std::endl;
-//            std::cout << "h: " << BloomFilter::hash(element, i) % this->m_bits << std::endl;
-//            std::cout << "at: " << this->storage.at(BloomFilter::hash(element, i) % this->m_bits) << std::endl;
-            unsigned long h = BloomFilter::hash(element, i);
-            //std::cout << "hash(" << element << ", " << i << "): " << h << std::endl;
-            unsigned long index = h % this->m_bits;
-            //std::cout << "contains index: " << index << std::endl;
+            unsigned long index = BloomFilter::hash(element, i) % this->m_bits;
             if (not this->storage.at(index)) {
                 return false;
             }
@@ -68,34 +58,6 @@ public:
 };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    BloomFilter hello(5, 3);
-//    std::cout << hello.storage.at(4) << std::endl;
-
-//    std::cout << "hashes" << std::endl;
-//    std::cout << BloomFilter::hash(1, 1) << std::endl;
-//    std::cout << BloomFilter::hash(1, 1) << std::endl;
-//    std::cout << BloomFilter::hash(1, 2) << std::endl;
-//    std::cout << BloomFilter::hash(2, 1) << std::endl;
-//    std::cout << BloomFilter::hash(2, 2) << std::endl;
-//    std::cout << BloomFilter::hash(1, 2) << std::endl;
-
-    std::cout << "contains" << std::endl;
-    std::cout << hello.contains(1) << std::endl;
-    hello.insert(1);
-    std::cout << hello.contains(1) << std::endl;
-    std::cout << hello.contains(2) << std::endl;
-//    std::cout << "list" << std::endl;
-//    for (int i = 0; i < hello.m_bits; ++i) {
-//        std::cout << hello.storage.at(i) << std::endl;
-//    }
-//    std::cout << "list inverted" << std::endl;
-//    hello.invert();
-//    for (int i = 0; i < hello.m_bits; ++i) {
-//        std::cout << hello.storage.at(i) << std::endl;
-//    }
-
-
     //// MPSI protocol
 
     /// Initialization
