e51a19f8ea7173a8e4967d8d69d933e85e9fd665
Jelle Vos <j.v.jelle@gmail.com>
2020-10-11 21:38:15 +0200

    Debug SCP

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <vector>
 #include "psi_protocols.h"
+#include "sub_protocols.h"
 
 
 // TODO: Cache n^2
 // TODO: Allow variable set sizes?
 int main() {
+    Keys keys;
+    key_gen(&keys, 512, 3, 4);
+    // TODO: Important! Rewrite to long instead of unsigned long, use upper half of modulus as negative range
+    std::cout << multiparty_comparison(encrypt(ZZ(3), keys.public_key),
+                                       encrypt(ZZ(5), keys.public_key),
+                                       3, keys) << std::endl;
+
     std::cout << "Computing the set intersection between multiple parties using a (2, 3)-encryption of 1024 bits."
     << std::endl;
 
--- sub_protocols.cpp
+++ sub_protocols.cpp
@@ -4,12 +4,20 @@
 
 #include "sub_protocols.h"
 
+ZZ random_number_below(const ZZ &n, const ZZ &below_x) {
+    NTL::ZZ ret;
+    while (true) {
+        ret = RandomBnd(below_x);
+        if (NTL::GCD(ret, n) == 1) { return ret; }
+    }
+}
+
 ZZ multiparty_comparison(ZZ a, ZZ b, unsigned long threshold_l, Keys &keys) {
     /// Designed in "Performance Comparison of Secure Comparison Protocols" by Kerschbaum, Biswas and De Hoogh
 
     // 1. Party X_1 computes the encryption of c = r(x_0 - x_1) - r', where r and r' are random
     ZZ r = Gen_Coprime(keys.public_key.n);
-    ZZ r_prime = Gen_Coprime(keys.public_key.n);  // TODO: Make sure r_prime < r
+    ZZ r_prime = random_number_below(keys.public_key.n, r);
 
     ZZ difference = subtract_homomorphically(a, b, keys.public_key);
     ZZ c_encrypted = subtract_homomorphically(multiply_homomorphically(difference, r, keys.public_key),
@@ -33,7 +41,7 @@ ZZ multiparty_comparison(ZZ a, ZZ b, unsigned long threshold_l, Keys &keys) {
         a_2 = rerandomize(a_2, keys.public_key);
 
         r = Gen_Coprime(keys.public_key.n);
-        r_prime = Gen_Coprime(keys.public_key.n);  // TODO: Make sure r_prime < r
+        r_prime = random_number_below(keys.public_key.n, r);
 
         c_encrypted = multiply_homomorphically(c_encrypted, ZZ((b_i * 2 - 1) * r), keys.public_key);
         c_encrypted = add_homomorphically(c_encrypted,
@@ -50,8 +58,22 @@ ZZ multiparty_comparison(ZZ a, ZZ b, unsigned long threshold_l, Keys &keys) {
     }
 
     if (combine_partial_decrypt(decryption_shares, keys.public_key) <= 0) {
+        std::vector<std::pair<unsigned long, ZZ>> decryption_sharesq;
+        decryption_sharesq.reserve(3);
+        for (int q = 0; q < (threshold_l + 1); ++q) {
+            decryption_sharesq.emplace_back(q + 1, partial_decrypt(a_1, keys.public_key,
+                                                                   keys.private_keys.at(q)));
+        }
+        std::cout << combine_partial_decrypt(decryption_sharesq, keys.public_key) << std::endl;
         return a_1;
     } else {
+        std::vector<std::pair<unsigned long, ZZ>> decryption_sharesq;
+        decryption_sharesq.reserve(3);
+        for (int q = 0; q < (threshold_l + 1); ++q) {
+            decryption_sharesq.emplace_back(q + 1, partial_decrypt(a_2, keys.public_key,
+                                                                   keys.private_keys.at(q)));
+        }
+        std::cout << combine_partial_decrypt(decryption_sharesq, keys.public_key) << std::endl;
         return a_2;
     }
 }
